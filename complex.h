fb190c56344723eab2a93308d081ca6d05de265e
ultrakl <ultrakuli258@gmail.com>
2023-08-18 23:27:02 +0800

    2023/8/18

--- complex.h
+++ complex.h
@@ -1,14 +1,18 @@
 #ifndef _COMPLEX_
 #define _COMPLEX_
-
+#include<iostream>
 // inline complex& doapls (complex* ths,const complex& x);
 class complex{
 public: 
-    complex(double r=0,double i=0):re(r),im(i){};
+    complex(double r=0,double i=0):re(r), im(i){std::cout<<"default_ctor"<<'\n';}
+    complex(std::initializer_list<double>){std::cout<<"list_initial_ctor"<<'\n';}
+    complex(const complex& x) {std::cout<<"copy_ctor"<<'\n';}
     complex& operator += (const complex& r);
+    virtual void draw() {};
+    virtual ~complex(){};
 private:
     double re,im;
-    friend complex& doapls(complex* ths,const complex&x);
+    friend complex& doapls(complex* ths,const complex& x);
 };
 
 inline complex& doapls (complex *ths,const complex& x)
@@ -23,4 +27,10 @@ inline complex& complex::operator+=(const complex& r)
     return doapls(this,r);
 }
 
+class derived : public complex{
+    using complex::operator+=;
+    virtual void draw() const{
+        
+    }
+};
 #endif
--- test.cpp
+++ test.cpp
@@ -6,6 +6,14 @@ int main()
     complex a(2,2);
     complex b(2,1);
     complex c = complex(1,2);
+    complex d = {1,2};
     a+=b;
-    cout<<"NB"<<endl;
+    cout<<"success"<<endl;
+    int au=3;
+    complex* p = new complex();
+    cout<<sizeof(*p)<<endl;
+    int *i = &au;
+    cout<<sizeof(i)<<endl; 
+    delete p;
+    return 0;
 }
